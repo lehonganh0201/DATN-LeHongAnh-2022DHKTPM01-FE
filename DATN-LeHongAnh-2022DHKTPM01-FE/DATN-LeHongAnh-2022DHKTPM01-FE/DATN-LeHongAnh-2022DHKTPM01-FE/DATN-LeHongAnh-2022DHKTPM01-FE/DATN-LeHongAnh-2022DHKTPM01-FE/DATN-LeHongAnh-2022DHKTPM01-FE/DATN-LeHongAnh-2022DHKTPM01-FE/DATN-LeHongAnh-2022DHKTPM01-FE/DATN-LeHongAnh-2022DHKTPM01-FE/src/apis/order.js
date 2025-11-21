9175a3ed742cca28734dde3c2e387b862e88bbca
lehonganh0201 <lehonganh0201@users.noreply.github.com>
2025-11-21 15:47:58 +0000

    fix edit variant page

--- DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/src/apis/order.js
+++ DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/DATN-LeHongAnh-2022DHKTPM01-FE/src/apis/order.js
@@ -53,4 +53,52 @@ export const getReferenceOrder = async (reference) => {
     console.error("Lỗi ở lấy đơn hàng theo mã tham chiếu:", error);
     throw new Error("Lấy đơn hàng theo mã tham chiếu không thành công");
   }
-}
+};
+
+export const getOrders = async (data) => {
+  try {
+    const {
+      page,
+      size,
+      sortedBy,
+      sortDirection,
+      status,
+      minTotal,
+      maxTotal,
+      startDate,
+      endDate,
+    } = data;
+    const response = await request(axiosPrivate, {
+      method: "GET",
+      url: "/orders",
+      params: {
+        page,
+        size,
+        sortedBy,
+        sortDirection,
+        status,
+        minTotal,
+        maxTotal,
+        startDate,
+        endDate,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error("Lỗi ở lấy đơn hàng:", error);
+    throw new Error("Lấy đơn hàng không thành công");
+  }
+};
+
+export const updateStatus = async (data) => {
+  try {
+    const { status, orderId } = data;
+    await request(axiosPrivate, {
+      method: "PUT",
+      url: `/orders/${orderId}?status=${status}`,
+    });
+  } catch (error) {
+    console.error("Lỗi ở cập nhật trạng thái:", error);
+    throw new Error("Cập nhật trạng thái không thành công");
+  }
+};
